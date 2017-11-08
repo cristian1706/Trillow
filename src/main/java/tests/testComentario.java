c23cf70363d08e53555b95073230ca3dddb6b775
pilicercato <mpilicercato@gmail.com>
2017-11-08 17:36:35 -0300

    test Comentario

--- src/main/java/tests/testComentario.java
+++ src/main/java/tests/testComentario.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 import model.Comentario;
+import model.Usuario;
 
 public class testComentario {
 	
@@ -18,13 +19,20 @@ public class testComentario {
 		etx.begin();
 				//Creo un mensaje y lo persisto
 		Comentario comen = new Comentario();
-		comen.setTexto("Soy un EJB3.0");
+		comen.setTexto("Soy un comentario");
 		comen.setId_usuario(1);
 		em.persist(comen);
 		//si ahora hiciéramos m.setTexto(“Otro texto”); este cambio también sería persistido
 		//Obtengo todos los mensajes
-		List<Comentario> comentarios=(List<Comentario>)(em.createQuery("from modelo.Comentario c order by c.texto asc")).getResultList();
-		for (Comentario com:comentarios) {System.out.println("Comentario : " +com.getTexto());}
+		List<Comentario> comentarios=(List<Comentario>)(em.createQuery("from modelo.Comentario")).getResultList();
+		List<Usuario> usuarios=(List<Usuario>)(em.createQuery("from modelo.Usuario")).getResultList();
+		for (Comentario com:comentarios) {
+			for (Usuario user:usuarios) {
+				if(com.getId_usuario()==user.getId()) {
+					System.out.println("Usuario: "+user.getUsername()+" Comentario : " +com.getTexto());
+				}
+			}
+		}
 		etx.commit();
 		em.close();
 		
